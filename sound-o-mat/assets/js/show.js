4e6e6d5dc54c3224496fce7ac8d412ea2c2fd110
RobVaughn <995447+RobVaughn@users.noreply.github.com>
2019-07-16 17:41:39 -0700

    Added services page, updated menu, related images and audio files

--- sound-o-mat/assets/js/show.js
+++ sound-o-mat/assets/js/show.js
@@ -1,4 +1,8 @@
-function showText() {
-  var x = document.getElementById("hidden-section");
+function showText(id) {
+  var x = document.getElementById(id);
   x.style.display = "block";
+}
+function hideText(id) {
+  var x = document.getElementById(id);
+  x.style.display = "display";
 }
