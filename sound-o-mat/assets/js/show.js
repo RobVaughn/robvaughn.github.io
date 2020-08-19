64bd9b446974926a800d82cf904d55deebd3bcc5
Rob V <995447+RobVaughn@users.noreply.github.com>
2020-08-19 07:18:04 -0700

    Added jQuery code for dynamic paragraph padding to make info cards equal length on desktops/screens vs. no padding for mobiles in portrait orientation

--- sound-o-mat/assets/js/show.js
+++ sound-o-mat/assets/js/show.js
@@ -1,8 +1,15 @@
 function showText(id) {
-  var x = document.getElementById(id);
-  x.style.display = "block";
+    var x = document.getElementById(id);
+    x.style.display = "block";
 }
+
 function hideText(id) {
-  var x = document.getElementById(id);
-  x.style.display = "display";
+    var x = document.getElementById(id);
+    x.style.display = "display";
+}
+
+function addBreaks(id, n) {
+    for (i = 0; i < n; i++) {
+	$(id).append( "<br>" );
+    }
 }
