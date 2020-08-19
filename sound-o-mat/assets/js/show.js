017a680a7de312c3681d70bdbbec1dafd518c17b
Rob V <995447+RobVaughn@users.noreply.github.com>
2020-08-19 15:40:29 -0700

    Tests

--- sound-o-mat/assets/js/show.js
+++ sound-o-mat/assets/js/show.js
@@ -9,7 +9,7 @@ function hideText(id) {
 }
 
 function addBreaks(id, n) {
-    $(document).ready(function() { window.console && console.log('Doc width: ', $(document).width()); });
+    $(document).ready(function() { console.log('Doc width: ', $(document).width()); });
     if ($(document).width() > 400) {
 	for (i = 0; i < n; i++) {
 	    $(id).append( "<br>" );
