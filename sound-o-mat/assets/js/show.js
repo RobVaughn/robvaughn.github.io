4f7d2840348bfeac317f1b8282e331658fcf1d68
Rob V <995447+RobVaughn@users.noreply.github.com>
2020-08-20 08:55:05 -0700

    Returning to original tack of using CSS, which is a better solution than jQuery

--- sound-o-mat/assets/js/show.js
+++ sound-o-mat/assets/js/show.js
@@ -7,16 +7,3 @@ function hideText(id) {
     var x = document.getElementById(id);
     x.style.display = "display";
 }
-
-function addBreaks(id, n) {
-    var msg = 'Doc width: ' + $(document).width();
-    alert(msg);
-    log(msg);
-    //$(document).ready(function() { log('Doc width: ' + $(document).width()); });
-    var e = document.getElementById(id);
-    if ($(document).width() > 400) {
-	e.style.display = "block";
-    } else {
-	e.style.display = "none";
-    }
-}
